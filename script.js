b763704a5dca5f1cde81153ce781f5ac8668fc63
nathanmdj <nathanmdj628@gmail.com>
2024-01-12 01:03:19 +0800

    risk calcu

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateRisk() {
+  const entryPrice = parseFloat(document.getElementById('entryP').value);
+  const riskAmount = parseFloat(document.getElementById('riskAmount').value);
+  const stopLoss = parseFloat(document.getElementById('stopLoss').value);
+  const leverage = parseFloat(document.getElementById('leverage').value);
+
+  if (isNaN(entryPrice) || isNaN(riskAmount) || isNaN(stopLoss)) {
+    document.getElementById('result').innerText = 'Please enter valid numbers.';
+    return;
+  }
+
+  const stopLossPercent = Math.abs((entryPrice - stopLoss)) / entryPrice
+  console.log(stopLossPercent)
+
+  const positionSize = riskAmount / (stopLossPercent * leverage);
+  console.log(positionSize);
+
+  document.getElementById('result').innerText = `Position Size: $${positionSize.toFixed(2)}`;
+}
